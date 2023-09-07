a00ee8be0db04e0cd92b9d9d043f8dcd5cf95625
David Monti <dmonti@zennonbi.com>
2023-09-07 18:31:32 -0300

    Commit chatbot

--- Servidor.py
+++ Servidor.py
@@ -3,6 +3,36 @@ import socket
 HOST = "127.0.0.1"  
 PORT = 54321  
 
+def Menu(dato):
+    if dato.startswith('/'):
+        dato = dato[1:]
+    comando = dato
+    switch(comando)
+
+def switch(comando):
+    if comando == "exit":
+        salir(comando)
+    elif comando == "direccion":
+        direccion()
+    elif comando == "login":
+        usuario()
+    elif comando == "mute":
+        mute()
+    elif comando == "todos":
+        todos()
+
+def salir(comando):
+    print(comando)
+    exit()
+def direccion():
+    print("cliente pide direccion")
+def usuario():
+    print("cliente pide loguearse")
+def mute():
+    print("mute")
+def todos():
+    print("todos")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -12,6 +42,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024)
             if data.decode('utf-8').startswith("/"):
+                Menu(data.decode('utf-8'))
                 print(f'Comando recibido!','Cliente dice:',data.decode('utf-8'))
                 resp = input('Servidor:')
                 conn.sendall(bytes(resp, encoding="ascii"))
